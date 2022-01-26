17c15ace132232e47e12a10cdd9bf08195e8ad6f
Max von Webel <max@343max.de>
2022-01-26 10:41:00 +0100

    Initial Commit

---
+++ Our Home/Our_HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Our_HomeApp.swift
+//  Our Home
+//
+//  Created by Max von Webel on 26.01.22.
+//
+
+import SwiftUI
+
+@main
+struct Our_HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
