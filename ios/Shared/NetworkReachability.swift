bbe37125dbe93dc00644ab6c0baea2f0c17004ef
Max von Webel <max@343max.de>
2022-09-04 13:04:15 +0200

    fixed network reachability

--- ios/Shared/NetworkReachability.swift
+++ ios/Shared/NetworkReachability.swift
@@ -21,9 +21,9 @@ class NetworkReachability: ReachabilityProvider {
   required convenience init(distance: Distance) {
     switch distance {
     case .Nearby:
-      self.init(hostName: Home.externalHost.host!)
-    case .Remote:
       self.init(hostName: Home.localNetworkHost.host!)
+    case .Remote:
+      self.init(hostName: Home.externalHost.host!)
     }
   }
   
