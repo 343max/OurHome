9d80b9adbd119cfe81483aec08815d4bb3e95a81
Max von Webel <max@343max.de>
2023-12-22 14:07:25 +0100

    Add auto reconnect to serialBuzzer. Doesn't work inside docker container yet

--- server/src/lib/buzzer.ts
+++ server/src/lib/buzzer.ts
@@ -6,30 +6,68 @@ export type Buzzer = {
   registerDoorbellHandler: (handler: () => Promise<"success" | "failure">) => void
 }
 
+type PortRef = {
+  port: SerialPort
+}
+
+const pingPong = async (portRef: PortRef, parser: ReadlineParser, reconnectCallback: () => void) => {
+  let gotPong = true
+
+  setInterval(() => {
+    if (!gotPong) {
+      reconnectCallback()
+    }
+    gotPong = false
+    portRef.port.write("ping\r\n")
+  }, 1000)
+
+  parser.on("data", (data) => {
+    if (data === "pong") {
+      gotPong = true
+    }
+  })
+}
+
 export const serialBuzzer = (serialPort: string): Buzzer => {
   let doorbellHandler: (() => Promise<"success" | "failure">) | null = null
 
-  const port = new SerialPort({
-    path: serialPort,
-    baudRate: 9600,
-  })
+  const parser = new ReadlineParser({ delimiter: "\r\n" })
 
-  const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }))
+  const setupPort = () => {
+    const port = new SerialPort({
+      path: serialPort,
+      baudRate: 9600,
+    })
+    port.pipe(parser)
+
+    port.on("error", (err) => {
+      console.error("Serial port error: ", err)
+    })
+
+    return port
+  }
+
+  const portRef: PortRef = { port: setupPort() }
+
+  pingPong(portRef, parser, () => {
+    console.log("should reconnect")
+    portRef.port.close()
+    portRef.port = setupPort()
+  })
 
   parser.on("data", (data) => {
-    console.log(`[buzzer] ${data}`)
     if (data === "ring") {
       doorbellHandler?.()
+    } else if (data === "pong") {
+      // ignore
+    } else {
+      console.log(`[buzzer] ${data}`)
     }
-  })
-
-  port.on("error", (err) => {
-    console.error("Serial port error: ", err)
   })
 
   return {
     pressBuzzer: async () => {
-      port.write("buzz\r\n")
+      portRef.port.write("buzz\r\n")
     },
     registerDoorbellHandler: (handler) => {
       doorbellHandler = handler
